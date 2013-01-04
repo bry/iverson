6a9f6d0205d90b9cab21b264ca740fec99c77ea5
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-03 23:00:01 -0800

    Manually created ruby tree and traversal order methods

---
+++ ruby/trees/manual_tree_main.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+
+require 'tree'
+
+# Construct node tree
+root = Node.new(100,nil,nil)
+
+# puts root.hasleft?
+
+left = Node.new(50,nil,nil)
+root.left = left
+
+right  = Node.new(110, nil, nil)
+root.right = right
+
+leftchild = Node.new(10, nil, nil)
+rightchild = Node.new(75, nil, nil)
+
+left.left = leftchild
+left.right = rightchild
+
+# puts root.hasleft?
+
+tree = Tree.new(root)
+
+# Output tree traversal orders
+values = []
+
+tree.traversePreOrder.each do |node|
+   values << node.value
+end
+
+puts "PreOrder: " 
+p values
+values = []
+
+tree.traverseInOrder.each do |node|
+   values << node.value
+end
+
+puts "InOrder: " 
+p values
+values = []
+
+tree.traversePostOrder.each do |node|
+   values << node.value
+end
+
+puts "PostOrder: " 
+p values
---
+++ ruby/trees/tree.rb
@@ -0,0 +1,90 @@
+class Node
+
+  attr_accessor :value, :left, :right
+
+  def initialize(value,left=nil,right=nil)
+    @value,@left,@right = value,left,right 
+  end
+
+  def hasleft?
+    if @left != nil
+      true
+    else
+      false
+    end
+  end
+
+  def hasright?
+    if @right != nil
+      true
+    else
+      false
+    end
+  end
+end
+
+class Tree
+
+  attr_accessor :root
+
+  def initialize(root)
+    @root = root
+  end
+
+  # Depth first traversal
+  def traverseInOrder
+     # Clear array
+     @@orderedNodes = [] 
+     traverseInOrderRecurse(root)
+     @@orderedNodes
+  end
+
+  def traversePreOrder
+     # Clear array
+     @@orderedNodes = [] 
+     traversePreOrderRecurse(root)
+     @@orderedNodes
+  end
+
+  def traversePostOrder
+     # Clear array
+     @@orderedNodes = [] 
+     traversePostOrderRecurse(root)
+     @@orderedNodes
+  end
+
+  # Recursion traversal functions 
+  private
+  
+  def traverseInOrderRecurse(root)
+    if root == nil
+      return
+    else
+      traverseInOrderRecurse(root.left)
+      # Add to static array
+      @@orderedNodes << root 
+      traverseInOrderRecurse(root.right)
+    end
+  end
+  
+  def traversePreOrderRecurse(root)
+    if root == nil
+      return
+    else
+      @@orderedNodes << root 
+      traversePreOrderRecurse(root.left)
+      traversePreOrderRecurse(root.right)
+    end
+  end
+
+  def traversePostOrderRecurse(root)
+    if root == nil
+      return
+    else
+      traversePostOrderRecurse(root.left)
+      traversePostOrderRecurse(root.right)
+      @@orderedNodes << root 
+    end
+  end
+
+end
