b35b20e929aa071c9265d742b7d49f8c1d8b1f9c
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-10 16:39:17 -0800

    Cleanup and refactor unit tests

--- ruby/solutions/compound_words/unit_tests.rb
+++ ruby/solutions/compound_words/unit_tests.rb
@@ -16,8 +16,6 @@ class TestCompoundWordSearcher < Test::Unit::TestCase
     end
 
     # Preprocess dictionary
-    # Build dictionary word prefix trie with word array for 
-    # O(l), where l is length of word, lookup time
     @trie = Trie.new(@words)
 
     # Use custom searcher
@@ -36,42 +34,20 @@ class TestCompoundWordSearcher < Test::Unit::TestCase
     word = "strawberryyanyan"
     assert_equal(false, @searcher.compound_word?(word, @trie))
   end
-=begin
-  (0..10).each do |i|
-    define_method("test_random_compound_word_#{i}") do
-      # Construct a valid compound word with random words from 
-      # words list
-      compound_word_size = Random.rand(2..3)
-      j = 0 
-      while j < compound_word_size
-        word = "#{word}#{@words[Random.rand(0..@words.length-1)]}"
-        j = j + 1
-      end
-    
-      # These constructed compound words should all test to true
-      puts "Testing random compound word: #{word}"
-      assert_equal(true, @searcher.compound_word?(word, @trie), "Fail word: #{word}")
-    end
-  end
-=end
-  def test_generate_random_compound_words 
 
+  def test_generate_random_compound_words 
     max_iterations = 10000
-    i = 0
-    while(i < max_iterations)
-      # Construct a valid compound word with random words from 
-      # words list
+
+    for i in 0..max_iterations
+      # Construct a valid compound word from words init in setup 
       compound_word_size = Random.rand(2..10)
-      j = 0
-      while j < compound_word_size
+      for j in 0..compound_word_size
         word = "#{word}#{@words[Random.rand(0..@words.length-1)]}"
-        j = j + 1
       end 
 
       # These constructed compound words should all test to true
       puts "Testing random compound word: #{word}"
       assert_equal(true, @searcher.compound_word?(word, @trie), "Fail word: #{word}")
-      i = i + 1
       word = ""
     end
   end
