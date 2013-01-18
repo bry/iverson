084970f7a4635b36cdf10038bd5ebc24ed533fc5
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-17 16:37:29 -0800

    Trying pre-release edge Ruby 2.0 features from Ruby 2.0.0-preview1 branch

---
+++ ruby_2.0/README.md
@@ -0,0 +1,4 @@
+Ruby 2 - edge ruby
+=======
+
+http://qiita.com/items/babfc9eb4751a3949f10
---
+++ ruby_2.0/edge_ruby.rb
@@ -0,0 +1,54 @@
+#!/Users/bcabalo/.rvm/rubies/ruby-2.0.0-preview1/bin/ruby
+
+puts "Ruby 2"
+
+# Refinements (localized monkey patching)
+module MyStringRefinement
+  refine String do 
+    def number?
+      match(/\d+/) ? true : false
+    end
+  end
+end
+
+begin
+  "22".number? #=> #<NoMethodError: undefined method `number?' for "22":String>
+rescue => e
+  p e #=>
+end
+
+
+module MyStringRefinement
+  p "22".number? # => true
+end
+
+
+module MyApp
+  using MyStringRefinement
+
+  p "123".number? # => true
+  p "foo".number? # => false
+end
+
+
+# %i literal for symbol array
+p %i{hello world whoop there it is}
+
+
+# Keyword arguments
+def word_wrap(string, before: "<", after: ">")
+  "#{before}#{string}#{after}"
+end
+
+p word_wrap("tag")
+p word_wrap("tag",before: "#<")
+p word_wrap("tag", after: "]")
+p word_wrap("tag", after: "]", before: "[")
+
+def capture_params(**params)
+  params
+end
+
+p capture_params(foo:"bar", hello:"world")
+
+
