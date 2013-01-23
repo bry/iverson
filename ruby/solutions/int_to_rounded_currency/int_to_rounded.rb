8289150aa73911eec9d48bcc6815d58b5b0bf0d2
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-22 20:30:48 -0800

    Initial Integer to Rounded Currency solution (monkey patched Fixnum with to_rounded_s method)

---
+++ ruby/solutions/int_to_rounded_currency/int_to_rounded.rb
@@ -0,0 +1,51 @@
+class Fixnum
+
+  def to_rounded_s
+    integer = self
+    negative_flag = false
+
+    if integer < 0
+      negative_flag = true 
+      integer = integer.abs
+    end
+
+    value = 0
+    if integer == 0
+      value = 0
+    elsif integer < 1000
+      value = integer
+    elsif integer < 10000
+      value = integer / 1000.0
+      value = value.round(1)
+      letter = "K"
+    elsif integer < 100000
+      value = integer / 1000
+      letter = "K"
+    elsif integer < 1000000
+      value = integer / 1000
+      letter = "K"
+    elsif integer < 10000000
+      value = integer / 1000000.0
+      value = value.round(1)
+      letter = "M"
+    elsif integer < 10000000000
+      value = integer / 1000000000.0
+      value = value.round(1)
+      letter = "B"
+    elsif integer < 1000000000000000
+      value = integer / 1000000000000
+      letter = "T"
+    end
+
+    if value == 0.0
+      value = 0 
+    end
+
+    if negative_flag 
+      return "($#{value}#{letter})"
+    else
+      return "$#{value}#{letter}"
+    end
+
+  end
+end 
