698b8143dd6d686f77a105d3d6e9079976c8bb89
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-22 23:04:14 -0800

    Brute force range test, random selection range test

--- ruby/solutions/int_to_rounded_currency/unit_tests.rb
+++ ruby/solutions/int_to_rounded_currency/unit_tests.rb
@@ -24,4 +24,20 @@ class TestIntToRounded < Test::Unit::TestCase
     assert_equal("$34T", 34223000121002.to_rounded_s )
     assert_equal("$Number out of range", 111119223000121002.to_rounded_s )
   end
+
+  def test_brute_force_to_rounded_s
+    puts "Testing 10M - all numbers from range"
+    (10000000..10999999).each do |x|
+      assert_equal("$10M", x.to_rounded_s )
+    end
+  end
+
+  def test_random_number_range_to_rounded_s
+    max_iterations = 2500000
+    puts "Testing 100T - #{max_iterations} random selections from range"
+    for i in 0..max_iterations
+      x = Random.rand(100000000000000..100999999999999)
+      assert_equal("$100T", x.to_rounded_s )
+    end
+  end
 end
