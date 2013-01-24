336f790636b1e78fc44bafa62605ae999a1fbba3
Bryan B. Cabalo <bcabalo@gmail.com>
2013-01-23 21:24:14 -0800

    Refactored if-elsif block to more readable case-when block

--- ruby/solutions/int_to_rounded_currency/unit_tests.rb
+++ ruby/solutions/int_to_rounded_currency/unit_tests.rb
@@ -12,7 +12,6 @@ class TestIntToRounded < Test::Unit::TestCase
     assert_equal("$501K", 501200.to_rounded_s)
     assert_equal("($1.2M)", -1240123.to_rounded_s)
     assert_equal("$5.9B", 5853010238.to_rounded_s)
-    # Additional test cases
     assert_equal("$124T", 124123000121002.to_rounded_s)
     assert_equal("$999T", 999123000121002.to_rounded_s)
     assert_equal("$999T", 999923000121002.to_rounded_s)
@@ -46,21 +45,16 @@ class TestIntToRounded < Test::Unit::TestCase
     puts "Testing proper result format - #{max_iterations} random selections from full range"
     for i in 0..max_iterations
       x = Random.rand(-999999999999999..999999999999999)
-
       temp_x = x.abs
-      valid_rounded_s = //
-      if temp_x == 0
-        valid_rounded_s = /\$[0-9]{0,3}[KMBT]{0}/ 
-      elsif temp_x > 0 and temp_x < 1000
-        valid_rounded_s = /\$[0-9]{0,3}[KMBT]{0}/ 
-      elsif temp_x >= 1000 and temp_x < 1000000
-        valid_rounded_s = /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[K]{1}/ 
-      elsif temp_x >= 1000000 and temp_x < 1000000000
-        valid_rounded_s = /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[M]{1}/ 
-      elsif temp_x >= 1000000000 and temp_x < 1000000000000
-        valid_rounded_s = /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[B]{1}/ 
-      elsif temp_x >= 1000000000000 and temp_x < 1000000000000000
-        valid_rounded_s = /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[T]{1}/ 
+
+      valid_rounded_s = case temp_x
+        when 0 then /\$[0-9]{0,3}[KMBT]{0}/ 
+        when 0...1000 then /\$[0-9]{0,3}[KMBT]{0}/ 
+        when 1000...1000000 then /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[K]{1}/ 
+        when 1000000...1000000000 then /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[M]{1}/ 
+        when 1000000000...1000000000000 then /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[B]{1}/ 
+        when 1000000000000...1000000000000000 then /\$[0-9]{0,3}\.{0,1}[0-9]{0,1}[T]{1}/ 
+        else "Integer out of range"
       end
 
       assert_match(valid_rounded_s, x.to_rounded_s) 
