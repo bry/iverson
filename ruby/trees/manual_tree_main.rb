b11a0756793e339294fbc27687af015bb936cf25
Bryan B. Cabalo <bcabalo@gmail.com>
2018-02-17 16:43:14 -0500

    Refactor main tree traversal script

--- ruby/trees/manual_tree_main.rb
+++ ruby/trees/manual_tree_main.rb
@@ -1,26 +1,16 @@
-#!/usr/bin/ruby
-
 require './tree'
 require './node'
 
 # Construct node tree
 root = Node.new(100,nil,nil)
-
-# puts root.hasleft?
-
 left = Node.new(50,nil,nil)
 root.left = left
-
 right  = Node.new(110, nil, nil)
 root.right = right
-
 leftchild = Node.new(10, nil, nil)
 rightchild = Node.new(75, nil, nil)
-
 left.left = leftchild
 left.right = rightchild
-
-# puts root.hasleft?
 
 tree = Tree.new(root)
 
@@ -31,21 +21,22 @@ tree.traversePreOrder.each do |node|
    values << node.value
 end
 
-puts "PreOrder: " 
+puts "PreOrder: "
 p values
-values = []
+values.clear
 
 tree.traverseInOrder.each do |node|
    values << node.value
 end
 
-puts "InOrder: " 
+puts "InOrder: "
 p values
-values = []
+values.clear
 
 tree.traversePostOrder.each do |node|
    values << node.value
 end
 
-puts "PostOrder: " 
+puts "PostOrder: "
 p values
+values.clear
