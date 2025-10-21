8880714eebfd6ac59ac259e93542a630ff3e5f56
Bryan B. Cabalo <bcabalo@gmail.com>
2025-10-21 00:36:35 -0700

    Ractor profile results on Apple M3 12-Core machine

--- ruby_3.5/ractor_profile.rb
+++ ruby_3.5/ractor_profile.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+
+# Ractor Performance on Apple M3 12-Core Hardware
+# 
+# ➜  ruby_3.5 git:(master) ✗ ruby ractor_profile.rb 
+# Calculating Fibonacci numbers: Single Process (Single Core)
+# (Time: 11.652601s)
+# Calculating Fibonacci numbers with Threads (Single Core)
+# (Time: 11.661066s)
+# Calculating Fibonacci numbers with OS Processes (Multicore Utilization)
+# (Time: 4.643127s)
+# Calculating Fibonacci numbers with Ruby 3.5 Ractors (Multicore Utilization)
+# ractor_profile.rb:47: warning: Ractor is experimental, and the behavior may change in future versions of Ruby! Also there are many implementation issues.
+# (Time: 4.80191s)
+
+
 def fib(n)
   return n if n < 2
   fib(n - 1) + fib(n - 2)
